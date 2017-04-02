34b680d9b7dadfb200252436674a0cb1ab9e6aef
imcort <179376503@qq.com>
2017-04-02 12:15:51 +0800

    SqStack Complete

--- main.cpp
+++ main.cpp
@@ -11,35 +11,41 @@
 #include "SqList.h"
 #include "SqStack.h"
 
-void conversion(){
+void conversion()
+{
     SqStack S;
     InitStack(S);
     int N;
-    std::cin>>N;
-    while(N){
-        Push(S, N%8);
-        N = N/8;
+    std::cin >> N;
+    while (N)
+    {
+        Push(S, N % 8);
+        N = N / 8;
     }
-    while(!StackEmpty(S)){
+    while (!StackEmpty(S))
+    {
         int e;
         Pop(S, e);
-        std::cout<<e;
+        std::cout << e;
     }
 }
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char *argv[])
+{
     SqStack sq;
     InitStack(sq);
-    for(int i=1;i<=50;i++)
-        Push(sq, i*2);  //插入50个数
-    for(int i=0;i<50;i++){
+    for (int i = 1; i <= 50; i++)
+        Push(sq, i * 2); //插入50个数
+    for (int i = 0; i < 50; i++)
+    {
         int e;
         Pop(sq, e);
-        std::cout<<e<<',';
+        std::cout << e << ',';
     }
-    std::cout<<std::endl<<"Dec to Oct:";
+    std::cout << std::endl
+              << "Dec to Oct:";
     conversion();
-    std::cout<<std::endl;
-    
+    std::cout << std::endl;
+
     return 0;
 }
